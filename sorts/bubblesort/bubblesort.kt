5e38fb0755ebb6ee791698d2c2bf911a430174a3
JOHN DERIGGI <JOHN.DERIGGI@GMAIL.COM>
2019-01-02 00:01:56 -0500

    comments

--- sorts/bubblesort/bubblesort.kt
+++ sorts/bubblesort/bubblesort.kt
@@ -5,11 +5,13 @@ import java.util.Collections
 
 fun main(args: Array<String>){
     
+    // our random maker
     val randomSize = Random(Instant.now().toEpochMilli());
     
     // make a rando sized array
     var listy = IntArray(100,{_ -> randomSize.nextInt(1000)})
     
+    // set the limit for the right side of the search
     var max = listy.size - 2
     for(i in 0..max){
         for( cursor in 0..max-i){
@@ -24,6 +26,7 @@ fun main(args: Array<String>){
      
 }
 
+// swap the values at two indices
 fun swap(arr: IntArray, b: Int, c: Int){
     var temp = arr[b];
     arr[b] = arr[c]
