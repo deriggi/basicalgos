6e73dc136ec7f544c858395a3918ce3dec7b94e7
John DeRiggi <john.deriggi@gmail.com>
2020-05-15 18:43:22 -0700

    cousera

---
+++ coursera/intro/CourseraPA1/src/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+
+        
+    }
+    
+}
